904cd75c069c1fc10c4665ebb65b0d516ce23056
pranav2308 <pranav08@terpmail.umd.edu>
2019-07-12 20:01:51 -0400

    Adding local SubX data module

---
+++ Python/download_data/local_SubX_data.py
@@ -0,0 +1,89 @@
+import os
+import glob
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+
+def local_SubX_data(url, outdir, ftype, mod, inst, var, plev, yv, xv, ens, fen):
+    ysave = str(int(yv))
+    xsave = str(int(xv))
+    inFname = url+'.'+inst+'/.'+mod+'/.'+ftype+'/.'+var+'/dods'
+    remote_data = xr.open_dataarray(inFname)
+    
+    if len(remote_data.dims) == 6:
+        da = remote_data.sel(P=plev, M=ens, Y=yv, X=xv)
+    if len(remote_data.dims) == 5:
+        da = remote_data.sel(M=ens, Y=yv, X=xv)
+
+    # For some models the ensembles start at 0
+    esave = ens
+    if fen == 0.0:
+        esave = esave + 1
+        da.M.values = da.M.values + 1
+
+    outdir += ftype+'/'+mod+'/'+var+'/'+str(plev)+'/daily/ts/'
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    # Check if any files have been created so they are not downloaded again
+    filescreated = glob.glob(outdir+'*.e'+str(int(esave))+'.y'+ysave+\
+                         '.x'+xsave+'.nc')
+    nfilescreated = len(filescreated)
+    if nfilescreated != 0:
+        filescreated.sort()
+        # Remove last file created incase stopped while saving
+        _lastfile = filescreated[-1]
+        os.unlink(_lastfile)
+        # Find the date and index of the last created file
+        # Split the string by / and get the first 8 chars
+        _lastdate = _lastfile.split('/')[-1][0:8]
+        _lastyear = _lastdate[0:4]
+        _lastmonth = _lastdate[4:6]
+        _lastday = _lastdate[6:8]
+        ts = pd.Timestamp(_lastyear+'-'+_lastmonth+'-'+_lastday+' 00:00:00')
+        # Find the index of this in da.S
+        datesdf = da.S.to_dataframe()
+        _icstart = datesdf.index.get_loc(ts)
+    else:
+        _icstart = 0
+
+    da_S_len = len(da.S.values)
+    for ic in range(_icstart, da_S_len):
+        # Check if data exists for this start date otherwise skip
+        da2 = da.sel(S=da.S.values[ic])
+        if not np.all(np.isnan(da2)):
+            # Convert to a pandas.Timestamp to get year, month, data
+            date = pd.Timestamp(da.S.values[ic])
+            year = str(date.year)
+            # Use zfill to pad with 0
+            month = str(date.month).zfill(2)
+            day = str(date.day).zfill(2)
+
+            # if S is YYY-MM-DDT12 substract 12 hours
+            if date.hour == 12:
+                da2.S.values = da2.S.values - pd.Timedelta('12 hours')
+
+            # Out file name
+            ofname = year+month+day+'.e'+str(int(esave))+'.y'+ysave+'.x'+\
+                    xsave+'.nc'
+
+            # Data often finishes before end of file
+            if len(remote_data.dims) == 6:
+                try:
+                    da2 = da2.expand_dims('S').expand_dims('M').expand_dims('P').\
+                          expand_dims('Y').expand_dims('X')
+                except IndexError:
+                    exit('All data likely downloaded. You can check '+\
+                         inFname[:-4]+'#views to see if it can generate an image.')
+            else:
+                try:
+                    da2 = da2.expand_dims('S').expand_dims('M').expand_dims('Y').\
+                          expand_dims('X')
+                except IndexError:
+                    exit('All data likely downloaded. You can check '+\
+                         inFname[:-4]+'#views to see if it can generate an image.')            
+            # Save file
+            da2.to_netcdf(outdir+ofname)
+
+
+
